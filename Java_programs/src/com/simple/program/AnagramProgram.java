864c6e213335beed0f49f11e14c9ec39ca009b9d
pramo <pramo@host.docker.internal>
2022-04-22 16:48:55 +0530

    adding anagram program

--- Java_programs/src/com/simple/program/AnagramProgram.java
+++ Java_programs/src/com/simple/program/AnagramProgram.java
@@ -25,7 +25,7 @@ public class AnagramProgram {
 			System.out.println(">>>String are not anagram>>>");
 		}
 		
-		//2. using for loop
+		//2. using for loop 
 		boolean isAnagram=true;
 		for(int i=0,j=0; i<len1;i++,j++) {
 			
